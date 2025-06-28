642d66abe88dd9957afe658189d96db736ff26e9
Milton <milton_pinheiro@outlook.com>
2025-06-28 02:49:52 -0400

    Cooldown de skill implementado

--- MultiplayerHub/League/Champions/skill.gd
+++ MultiplayerHub/League/Champions/skill.gd
@@ -4,13 +4,26 @@ class_name Skill extends Node2D
 @export var unlockable = false
 @onready var champion = get_parent().get_parent().champion
 
+@export var cooldown = 1
+var cooldown_waiter: Timer
+
+func _ready():
+	cooldown_waiter = Timer.new()
+	cooldown_waiter.one_shot = true
+	add_child(cooldown_waiter)
+
 func try_cast():
 	if unlockable or !get_parent().get_parent().locked:
-		if champion.stats.mana > mana_cost:
-			champion.stats.mana -= mana_cost
-			cast()
+		if cooldown_waiter.is_stopped():
+			if champion.stats.mana > mana_cost:
+				cooldown_waiter.wait_time = cooldown
+				cooldown_waiter.start()
+				champion.stats.mana -= mana_cost
+				cast()
+			else:
+				out_of_mana()
 		else:
-			out_of_mana()
+			out_of_cooldown()
 	else:
 		print("ta lockado")
 
@@ -19,3 +32,6 @@ func cast():
 
 func out_of_mana():
 	print("Out of mana")
+
+func out_of_cooldown():
+	print("Out of cooldown")
